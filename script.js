78f2a345d8ea3ba133a22f72d342eba15c28b6b2
mr-leandev <mason@leandev.com.au>
2024-08-23 20:23:18 +1000

    Game logic constructed but runtime errors: archiving for version management

--- script.js
+++ script.js
@@ -1,102 +1,144 @@
-console.log("hello world,")
+console.log("Rock, paper, scissors is running...")
+console.log("Hello and welcome to my rock paper scissors console game");
+console.log("To play, enter 'y' - any other key will exit");
+//Initiate playGame as variable for while container managing runtime
+//Purpose: Manage sessions, avoid unwanted console prompts.
+const playGame = prompt("To play, enter 'y' - any other key will exit");
 
-//Define State controller
+    //If input != y , then Game exits to avoid repeated pop-ups
 
-//Pseudocode**
-//Track player Choice
-//When player choice is selected, generate computer choice. 
-//math random, divide into 3rds with each section representing a choice
-//evaluate which choice wins
-//need to add evaluation, probably neater using a called helper function
-//track score, break limit of 5 rounds, or first to 3. 
-
-
-    
-    console.log("welcome to my rock paper scissors console game");
-    console.log("To play, enter 'y' - any other key will exit");
-    //If you want to play, (to avoid unwanted console popups)
-    //Toggle variable to manage runtime, if playGame return is no, terminate function
-    //May need to introduce while statements to handle this
-    const playGame = prompt("To play, enter 'y' - any other key will exit");
-    console.log("playGame is: " + playGame);
-    while (playGame === "y") {
-
-//If game is not being played, print console message
-        if (playGame != "y"){
-            console.log("No worries. Reload the page if you would like to play.")
-        }
-
-        //Initialise Variables
-        //Track the number of "5 game rounds" that have been played
-        let roundsPlayed = 0;
-        //
-        let playerRoundsWon = 0;
-        let computerRoundsWon = 0;
-        //Track the win 
-        let playerGamesWon = 0;
-        let computerGamesWon = 0;
-
-        printScoreboard();
-
-//Game is being played
-//score must be initialised
-//rounds can be incremented at the end of game loop
-
-        
-        //Create a game loop
-        while (playRound === TRUE){
-
- 
-
-
-    
-
-    //Enable short-hand entry ["r","p","s"] by parsing prompt response.
-    let promptChoice = prompt("Rock, Paper, or Scissors? Enter r, p, s as your choice");
-    let choiceMade = ""; //Required for state management
-    let invalidChoice = "" //Required to handle invalid choices
-   
-    let playerChoice = parsePlayerChoice(promptChoice);
-    let computerChoice = Math.random();
-    evaluateChoice(playerChoice, computerChoice);
-
-
-
-
-
-    
-
-//Execute State Controller
-
-//
-        }
+    if (playGame != "y"){
+        alert("No worries. Reload the page if you would like to play.")
     }
 
+//Initialise Variables for main game runtime
 
+//Track the number of "5 game rounds" that have been played
+let roundsPlayed = 0;
+// Count the number of full 5 game rounds won for player/computer
+let playerRoundsWon = 0;
+let computerRoundsWon = 0;
+//Per round score tracking
+let playerCurrentScore = 0;
+let computerCurrentScore= 0;
+//Per round game tracking
+let gameNumber = 0; // Current game number 0 - 5
+let choiceMade = false; //Track successful choice is made
+let playRound = true; //State controller for rounds
+
+    //Main runtime container
     
+    while (playGame === "y") {
+
+        //Show initial scoreboard
+
+        printScoreboard();
+        
+            //Create a game loop
+            while (playRound === true){
+
+                //Setup conditions for exiting the game loop when game count is 5
+                //Evaluate winner and increment variables
+
+                console.log(gameNumber);
+
+            if (gameNumber >= 5){
+                console.log("Game number equal to 5")
+                //Pseucode
+                //if game number is above 5, means the round is complete
+                //evaluate the scores and assign a winner
+                //increment win count according to winner
+                //reset runtime to print scoreboard, add a "would you like to play a round?" prompt
+            //Neither player has a score of 3, automatically a draw
+                if (playerCurrentScore <= 2 && computerCurrentScore <=2){
+                    console.log("Round tied");
+                    roundsPlayed++;
+                    console.log("Rounds played " + roundsPlayed);
+                    gameNumber = 0;
+                    break;
+                }
+                if (playerCurrentScore >= 3){
+                    console.log("Player won!");
+                    roundsPlayed++;
+                    playerRoundsWon++;
+                    gameNumber = 0;
+                    console.log("Rounds played " + roundsPlayed);
+                    break;
+                }
+                if (computerCurrentScore >= 3){
+                    console.log("Computer won!");
+                    computerRoundsWon++;
+                    roundsPlayed++;
+                    gameNumber = 0;
+                    console.log("Rounds played " + roundsPlayed);
+                    break;
+                }
+                
 
 
+            }
 
+                while (choiceMade === false) {
+
+                    //Send a prompt requesting an input
+                    //Enable short-hand entry ["r","p","s"] by parsing prompt response.
+                    let promptChoice = prompt("Rock, Paper, or Scissors? Enter r, p, s as your choice. 'x' to terminate.");
+                
+                    //console.log("promptChoice is: " + promptChoice);
+
+                    //Parse the input to check it is valid, mark as choice made
+                    let playerChoice = parsePlayerChoice(promptChoice);
+
+                    //console.log("player choice: " + playerChoice);
+                    
+                    //Check the choice is a valid choice
+                    if (playerChoice === "Rock" || "Paper" || "Scissors"){choiceMade=true;}
+                    console.log("choiceMade variable is:" + choiceMade);
+                    
+
+                }
+                
+
+                    //User choice is valid, calculate the result
+                    if (choiceMade === true) {
+
+                        let computerChoice = getComputerChoice();
+                        //Evaluate winner and update score variables
+                        evaluateChoice(playerChoice, computerChoice);
+
+                        
+                        //Increment variables switch case statement
+                    }//End of game evaluation
+                
+
+    }//Play-round Exit
+}//Main Exit
+
+//
 //Called helper functions
-
+//
 
 function printScoreboard() {
-    console.log("Rounds Played: " + roundsPlayed);
-    console.log("Player Won: " + playerRoundsWon);
-    console.log("Computer Won: " + computerRoundsWon);
-    console.log("Player Current Score: " + playerGamesWon);
-    console.log("Computer Current Score : " + computerGamesWon);
+    console.log("You have played " + roundsPlayed + " Rounds.");
+    console.log("You have won " + playerRoundsWon + " of " + roundsPlayed + ".");
+    console.log("The computer has won " + computerRoundsWon + " of " + roundsPlayed + ".");
+    console.log("Player Current Score: " + playerCurrentScore);
+    console.log("Computer Current Score : " + computerCurrentScore);
 }
 
+
 //Parse player choice
-function parsePlayerChoice() {
-    let playerChoice = prompt("Choose Rock, Paper or Scissors: r, p, s. 'x' to exit.");
-    switch (playerChoice) {
+function parsePlayerChoice(promptChoice) {
+
+    switch (promptChoice) {
         case "r":
+            choiceMade = true;
             return "Rock";
         case "p":
+            choiceMade = true;
             return "Paper";
         case "s":
+            choiceMade = true;
             return "Scissors";
         case "x":
             playGame = "n";
@@ -108,14 +150,107 @@ function parsePlayerChoice() {
 
 function getComputerChoice(){
     let randomChoice = Math.random();
-
-    //Evaluate if or switch statements. if 
+    console.log("Computer random math is: " + randomChoice);
+    switch (true) {
+        case randomChoice >= 0 && randomChoice < 0.33:
+            return "Rock";
+        case randomChoice >= 0.33 && randomChoice < 0.66:
+            return "Paper";
+        case randomChoice >= 0.66 && randomChoice <= 1:
+            return "Scissors";
+        default:
+            console.log("math error");
+            return "Invalid choice";
+    }
 }
-
-
 
 function evaluateChoice(playerChoice, computerChoice) {
-    let winner = "";
-    return 
-}
+    
+    //Evaluation for ties
+    console.log("evaluating choice");
+    console.log("player choice is" + playerChoice);
+    console.log("computer choice is" + computerChoice);
+    if (playerChoice === computerChoice){ 
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log("Tie!!! Choose again!");
+        console.log("Your score: " + playerCurrentScore + ". Computer score: " + computerCurrentScore + ".");
+        return
+    }
 
+    // Evaluation logic for human wins
+
+
+    //Scissors beat paper (player)
+    if (playerChoice === "Scissors" && computerChoice === "Paper"){
+        playerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(playerChoice + " Beats " + computerChoice + "!" + " You win!!! :-)");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }   
+    //Paper beats rock (player)
+    if (playerChoice === "Paper" && computerChoice === "Rock"){
+        playerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(playerChoice + " Beats " + computerChoice + "!" + " You win!!! :-)");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }
+    //Rock beats scissors (player)
+    if (playerChoice === "Rock" && computerChoice === "Scissors"){
+        playerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(playerChoice + " Beats " + computerChoice + "!" + " You win!!! :-)");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }
+
+    //Evaluation logic for computer wins
+
+    //Scissors beat paper (player)
+    if (computerChoice === "Scissors" && playerChoice === "Paper"){
+        computerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(computerChoice + " Beats " + playerChoice + "!" + " Computer wins!!! :-(");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }   
+    //Paper beats rock (player)
+    if (computerChoice === "Paper" && playerChoice === "Rock"){
+        computerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(computerChoice + " Beats " + playerChoice + "!" + " Computer wins!!! :-(");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }
+    //Rock beats scissors (player)
+    if (computerChoice === "Rock" && playerChoice === "Scissors"){
+        computerCurrentScore++; 
+        gameNumber++;
+        console.log("Player chooses " + playerChoice);
+        console.log("Computer chooses " + computerChoice);
+        console.log(computerChoice + " Beats " + playerChoice + "!" + " Computer wins!!! :-(");
+        console.log("Your score is: " + playerCurrentScore + ". Computer score is: " + computerCurrentScore + ".");
+        
+    }
+
+    //(Remaining cases to handle)
+    //Scissors loses to rock
+    //Scissors loses to paper
+    //Rock loses to paper
+
+    return
+
+}
+    
